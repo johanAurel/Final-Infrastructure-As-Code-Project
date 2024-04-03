c7088166fa8daa5104e18fa881a6593697c82aee
Johan Aurel Botouli <aurel.botouli@gmail.com>
2024-04-03 09:23:37 +0100

    feat: update the sizes of the nodes

--- terraform/modules/containerisation/variables.tf
+++ terraform/modules/containerisation/variables.tf
@@ -15,7 +15,7 @@ variable "private_subnets" {
 
 variable "desired_size" {
   type = number
-  default = 2
+  default = 3
 }
 
 variable "min_size" {
@@ -25,7 +25,7 @@ variable "min_size" {
 
 variable "max_size" {
   type = number
-  default = 2
+  default = 4
 }
 
 variable "node_group_name" {
